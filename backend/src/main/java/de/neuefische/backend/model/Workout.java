a16f56da4f2e6e7e244cdd5eb94f87c8bf980020
Reebal Sami <reebal.sami@gmail.com>
2024-03-05 16:40:58 +0100

    add packages add model class Workout add repo WorkoutRepo

---
+++ backend/src/main/java/de/neuefische/backend/model/Workout.java
@@ -0,0 +1,16 @@
+package de.neuefische.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "workouts")
+public class Workout {
+    private String id;
+    private String name;
+    private String description;
+}
