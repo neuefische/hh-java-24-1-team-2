1b9dc76ab6b0dc82f468ecac98418f46b381cb12
jessi <jessi.nowski@hotmail.de>
2024-03-06 11:09:05 +0100

    add IntegrationsTest for deleteWorkoutById in ControllerTest

--- backend/src/test/java/de/neuefische/backend/controller/WorkoutControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/WorkoutControllerTest.java
@@ -1,12 +1,36 @@
 package de.neuefische.backend.controller;
 
+import de.neuefische.backend.repository.WorkoutRepo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class WorkoutControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private WorkoutRepo repo;
 
     @Test
-    void deleteWorkoutById() {
+    void deleteWorkoutById() throws Exception {
+        //GIVEN
+        //WHEN & THEN
+        mvc.perform(MockMvcRequestBuilders.delete("/api/workout/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
     }
 }
