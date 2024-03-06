5afa203a7b99fe4c6f4777a7973091b2942c5867
jessi <jessi.nowski@hotmail.de>
2024-03-06 10:30:55 +0100

    add UnitTest for deleteWorkoutById

---
+++ backend/src/test/java/de/neuefische/backend/controller/WorkoutControllerTest.java
@@ -0,0 +1,12 @@
+package de.neuefische.backend.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkoutControllerTest {
+
+    @Test
+    void deleteWorkoutById() {
+    }
+}
