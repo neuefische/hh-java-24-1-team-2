29a515a77fc284ee808b8b313b44af680cbbd2ef
BetulEzgu <33276357+betulEZ@users.noreply.github.com>
2024-03-06 12:31:16 +0100

    changed path2

--- backend/src/test/java/de/neuefische/backend/controller/WorkoutControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/WorkoutControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
