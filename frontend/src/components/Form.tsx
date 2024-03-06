3916738cb4178909f6767e4b8ad3a23675eac3d4
aaronbecher <Aaronbecher@gmx.de>
2024-03-06 12:00:21 +0100

    Installed yup and axios in the frontend Added directory components in the frontend Added Form.tsx and FormFields.tsx in the directory components

---
+++ frontend/src/components/Form.tsx
@@ -0,0 +1,10 @@
+import FormFields from './FormFields';
+
+export default function Form() {
+    return (
+        <>
+            <h2>Please input your workout here: </h2>
+            <FormFields />
+        </>
+    );
+}
