17a05d6c7e492905cb3512356af5f5e918c1418e
jessi <jessi.nowski@hotmail.de>
2024-03-07 10:43:10 +0100

    add function handleDelete

--- frontend/src/pages/DetailsPage.tsx
+++ frontend/src/pages/DetailsPage.tsx
@@ -1,4 +1,25 @@
-export default function DetailsPage() {
+import {Workout} from "../types/Workout.ts";
+import {useNavigate, useParams} from "react-router-dom";
+import axios from "axios";
+
+type DetailsPageProps={
+    workouts: Workout[],
+    fetchData: ()=>void
+}
+
+export default function DetailsPage(props: Readonly<DetailsPageProps>) {
+    const params = useParams();
+    const workout = props.workouts.find(workout => workout.id === params.id);
+    const navigate = useNavigate();
+
+    function handleDelete(){
+        if(workout !== undefined){
+            axios.delete("/api/workouts/"+workout.id)
+                .then(props.fetchData);
+            navigate("/workouts");
+        }
+
+    }
     return (
         <div>
             <h1>Details Page</h1>
