770839af188007be762853e27ee06b094d1d8e44
Reebal Sami <reebal.sami@gmail.com>
2024-03-07 12:51:51 +0100

    add frontend HomePage DetailsPage add backend getAllById in Controller and Service

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,42 @@
-export default function HomePage() {
+import {Workout} from "../types/Workout.ts";
+import {useEffect} from "react";
+import axios from "axios";
+import {Link} from "react-router-dom";
+
+type HomePageProps = {
+    workouts: Workout[]
+    setWorkouts: (workouts: Workout[]) => void
+}
+
+export default function HomePage(props: Readonly<HomePageProps>) {
+
+
+    function fetchWorkouts(){
+        axios.get("/api/workouts")
+            .then(response => {
+                props.setWorkouts(response.data)
+            })
+            .catch(error => {
+                console.error("Error fetching workouts", error)
+            })
+    }
+
+    useEffect(fetchWorkouts, []);
+    if (!props.workouts) {
+        return "Loading..."
+    }
     return (
         <div>
-            <h1>Home Page</h1>
-            <p>Welcome to the home page</p>
+            <h1>Workouts</h1>
+            <ul>
+                {props.workouts.map(workout => (
+                    <li key={workout.id}>
+                        <Link to={`/workouts/${workout.id}`}>
+                            {workout.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
-
-
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],   server: {
+  plugins: [react()],
+  server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080'
-      }
+      '/api': 'http://localhost:8080'
     }
   }
 })
