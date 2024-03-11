c41f97a848a27dc644b1546cf576c290da3d70d2
Reebal Sami <reebal.sami@gmail.com>
2024-03-11 14:08:22 +0100

    adjust request type to json

--- backend/src/main/java/de/neuefische/backend/service/OpenAiService.java
+++ backend/src/main/java/de/neuefische/backend/service/OpenAiService.java
@@ -1,21 +1,14 @@
 package de.neuefische.backend.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import de.neuefische.backend.model.WorkoutDto;
 import de.neuefische.backend.model.openai.ChatGptRequest;
 import de.neuefische.backend.model.openai.ChatGptResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
-import java.util.List;
-
 @Service
-
 public class OpenAiService {
     private final RestClient restClient;
-    private final ObjectMapper objectMapper;
 
     public OpenAiService(
                         @Value("${open.ai.url}") String apiUrl,
@@ -25,18 +18,16 @@ public class OpenAiService {
                 .baseUrl(apiUrl)
                 .defaultHeader("Authorization", apiKey)
                 .build();
-        objectMapper = new ObjectMapper();
     }
 
-    public List<WorkoutDto> askQuestion(String q) throws JsonProcessingException {
+    public String askQuestion(String q) {
         ChatGptRequest request = new ChatGptRequest(q);
         ChatGptResponse response = restClient.post()
                 .body(request)
                 .retrieve()
                 .body(ChatGptResponse.class);
         assert response != null;
-        return List.of(objectMapper.readValue(response.getAnswer(), WorkoutDto[].class));
+        return response.getAnswer();
     }
-
 
 }
