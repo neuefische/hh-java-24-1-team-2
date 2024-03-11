171e0a82730634d121a70df45bae33d90419ed9b
jessi <jessi.nowski@hotmail.de>
2024-03-11 19:34:16 +0100

    new Branch FilteredWorkouts

---
+++ frontend/src/components/FilteredWorkouts.tsx
@@ -0,0 +1,89 @@
+import {MuscleGroup, SportsCategory, Workout} from "../types/Workout.ts";
+import {FormControl, InputLabel, MenuItem, OutlinedInput, Select, SelectChangeEvent} from "@mui/material";
+import {ChangeEvent, useState} from "react";
+import {Link} from "react-router-dom";
+
+export type FilterWorkoutsProps={
+    workouts:Workout[];
+}
+export default function FilteredWorkouts(props: Readonly<FilterWorkoutsProps>){
+    const [searchName, setSearchName]=useState<string>("");
+    const [searchCategory, setSearchCategory]=useState<string>("");
+    const [searchMuscle, setSearchMuscle]=useState<string>("");
+
+    const optionalCategories=Object.values(SportsCategory);
+    const optionalMuscles=Object.values(MuscleGroup);
+
+    function handleSearchName(e: ChangeEvent<HTMLInputElement>){
+        const value = e.target.value;
+        setSearchName(value);
+    }
+
+    function handleSearchCategory(e: SelectChangeEvent<typeof searchCategory>){
+        setSearchCategory(e.target.value);
+    }
+
+    function handleSearchMuscle(e: SelectChangeEvent<typeof searchCategory>){
+        setSearchMuscle(e.target.value);
+    }
+
+    const filteredWorkouts=props.workouts.filter(workout=>
+        workout.name.includes(searchName) &&
+        workout.categories.toString().includes(searchCategory) &&
+        workout.muscleGroups.toString().includes(searchMuscle));
+
+
+    return(
+        <>
+            <label>
+                Search for:
+                <input type={"text"} value={searchName} onChange={handleSearchName}/>
+            </label>
+            <FormControl sx={{m: 1, width: 300}}>
+                <InputLabel>Choose category</InputLabel>
+                <Select
+                    value={searchCategory}
+                    onChange={handleSearchCategory}
+                    input={<OutlinedInput label="Choose category"/>}
+                >
+                    {optionalCategories.map(category =>
+                        <MenuItem
+                            key={category}
+                            value={category}
+                        >
+                            {category}
+                        </MenuItem>
+                    )}
+                </Select>
+            </FormControl>
+            <br/>
+            <FormControl sx={{m: 1, width: 300}}>
+                <InputLabel>Choose muscle</InputLabel>
+                <Select
+                    value={searchMuscle}
+                    onChange={handleSearchMuscle}
+                    input={<OutlinedInput label="Choose muscle"/>}
+                >
+                    {optionalMuscles.map(muscle =>
+                        <MenuItem
+                            key={muscle}
+                            value={muscle}
+                        >
+                            {muscle}
+                        </MenuItem>
+                    )}
+                </Select>
+            </FormControl>
+            <ul>
+                {filteredWorkouts.map(workout => (
+                    <li key={workout.id}>
+                        <Link to={`/workouts/${workout.id}`}>
+                            {workout.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+
+        </>
+    )
+}
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,5 @@
 import {Workout} from "../types/Workout.ts";
-import {ChangeEvent, useEffect, useState} from "react";
-import axios from "axios";
-import {Link} from "react-router-dom";
+import FilteredWorkouts from "../components/FilteredWorkouts.tsx";
 
 type HomePageProps = {
     workouts: Workout[]
@@ -9,47 +7,11 @@ type HomePageProps = {
 }
 
 export default function HomePage(props: Readonly<HomePageProps>) {
-const [search, setSearch]=useState<string>("");
-
-    function fetchWorkouts(){
-        axios.get("/api/workouts")
-            .then(response => {
-                props.setWorkouts(response.data)
-            })
-            .catch(error => {
-                console.error("Error fetching workouts", error)
-            })
-    }
-
-    useEffect(fetchWorkouts, []);
-    if (!props.workouts) {
-        return "Loading..."
-    }
-
-    function handleSearchInput(e: ChangeEvent<HTMLInputElement>){
-        const value = e.target.value;
-        setSearch(value);
-    }
-
-    const filteredWorkouts=props.workouts.filter(workout=> workout.name.includes(search) || workout.categories.toString().includes(search) || workout.muscleGroups.toString().includes(search));
-
 
     return (
         <div className={"homepage"} >
             <h1>Workouts</h1>
-            <label>
-                Search for:
-            <input type={"text"} value={search} onChange={handleSearchInput}/>
-            </label>
-            <ul>
-                {filteredWorkouts.map(workout => (
-                    <li key={workout.id}>
-                        <Link to={`/workouts/${workout.id}`}>
-                            {workout.name}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            <FilteredWorkouts workouts={props.workouts}/>
         </div>
     )
 }
