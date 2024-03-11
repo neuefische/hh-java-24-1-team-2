8328982f1e5e6e3942a467725ebe98d42eca0a55
jessi <jessi.nowski@hotmail.de>
2024-03-11 19:34:16 +0100

    new Branch FilteredWorkouts

--- frontend/src/components/FilteredWorkouts.tsx
+++ frontend/src/components/FilteredWorkouts.tsx
@@ -1,4 +1,5 @@
 import {MuscleGroup, SportsCategory, Workout} from "../types/Workout.ts";
+import {FormControl, InputLabel, MenuItem, OutlinedInput, Select, SelectChangeEvent} from "@mui/material";
 import {Autocomplete, TextField} from "@mui/material";
 import {ChangeEvent, useState} from "react";
 import {Link} from "react-router-dom";
@@ -19,6 +20,14 @@ export default function FilteredWorkouts(props: Readonly<FilterWorkoutsProps>){
         setSearchName(value);
     }
 
+    function handleSearchCategory(e: SelectChangeEvent<typeof searchCategory>){
+        setSearchCategory(e.target.value);
+    }
+
+    function handleSearchMuscle(e: SelectChangeEvent<typeof searchCategory>){
+        setSearchMuscle(e.target.value);
+    }
+
     const filteredWorkouts=props.workouts.filter(workout=>
         workout.name.includes(searchName) &&
         workout.categories.toString().includes(searchCategory) &&
@@ -29,6 +38,43 @@ export default function FilteredWorkouts(props: Readonly<FilterWorkoutsProps>){
         <>
             <label>
                 Search for:
+                <input type={"text"} value={searchName} onChange={handleSearchName}/>
+            </label>
+            <FormControl sx={{m: 1, width: 300}}>
+                <InputLabel>Choose category</InputLabel>
+                <Select
+                    value={searchCategory}
+                    onChange={handleSearchCategory}
+                    input={<OutlinedInput label="Choose category"/>}
+                >
+                    {optionalCategories.map(category =>
+                        <MenuItem
+                            key={category}
+                            value={category}
+                        >
+                            {category}
+                        </MenuItem>
+                    )}
+                </Select>
+            </FormControl>
+            <br/>
+            <FormControl sx={{m: 1, width: 300}}>
+                <InputLabel>Choose muscle</InputLabel>
+                <Select
+                    value={searchMuscle}
+                    onChange={handleSearchMuscle}
+                    input={<OutlinedInput label="Choose muscle"/>}
+                >
+                    {optionalMuscles.map(muscle =>
+                        <MenuItem
+                            key={muscle}
+                            value={muscle}
+                        >
+                            {muscle}
+                        </MenuItem>
+                    )}
+                </Select>
+            </FormControl>
                 <input type={"search"} value={searchName} onChange={handleSearchName}/>
             </label>
             <Autocomplete
