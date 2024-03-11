323575db14a5dd90580c12966085dae884fd95de
jessi <jessi.nowski@hotmail.de>
2024-03-11 10:00:55 +0100

    Separated Checkbox for Category and MuscleGroup in Comp

---
+++ frontend/src/components/CategoryMuscleCheckbox.tsx
@@ -0,0 +1,53 @@
+import {FormControl, InputLabel, MenuItem, OutlinedInput, Select, SelectChangeEvent} from "@mui/material";
+import {MuscleGroup, SportsCategory} from "../types/Workout.ts";
+
+export type CategoryMuscleCheckboxProps={
+    categories:string[],
+    muscleGroups: string[],
+    changeCategories: (event: SelectChangeEvent<string[]>)=>void,
+    changeMuscleGroups: (event: SelectChangeEvent<string[]>)=>void,
+}
+export default function CategoryMuscleCheckbox(props: Readonly<CategoryMuscleCheckboxProps>){
+    const optionalCategories=Object.values(SportsCategory);
+    const optionalMuscles=Object.values(MuscleGroup);
+
+    return(
+        <>
+            <FormControl sx={{ m: 1, width: 300 }}>
+                <InputLabel>Categories</InputLabel>
+                <Select
+                    multiple
+                    value={props.categories}
+                    onChange={props.changeCategories}
+                    input={<OutlinedInput label="Categories" />}
+                >
+                    {optionalCategories.map(category=>
+                        <MenuItem
+                            value={category}
+                        >
+                            {category}
+                        </MenuItem>
+                    )}
+                </Select>
+            </FormControl>
+            <br/>
+            <FormControl sx={{ m: 1, width: 300 }}>
+                <InputLabel>Muscle Groups</InputLabel>
+                <Select
+                    multiple
+                    value={props.muscleGroups}
+                    onChange={props.changeMuscleGroups}
+                    input={<OutlinedInput label="MuscleGroups" />}
+                >
+                    {optionalMuscles.map(muscle=>
+                        <MenuItem
+                            value={muscle}
+                        >
+                            {muscle}
+                        </MenuItem>
+                    )}
+                </Select>
+            </FormControl>
+        </>
+    )
+}
