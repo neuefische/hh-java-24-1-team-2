724d926d22600653a173c15f8b3462aaf4507433
jessi <jessi.nowski@hotmail.de>
2024-03-11 19:34:16 +0100

    new Branch FilteredWorkouts

--- frontend/src/components/FilteredWorkouts.tsx
+++ frontend/src/components/FilteredWorkouts.tsx
@@ -39,6 +39,7 @@ export default function FilteredWorkouts(props: Readonly<FilterWorkoutsProps>){
             <label>
                 Search for:
                 <input type={"search"} value={searchName} onChange={handleSearchName}/>
+                <input type={"text"} value={searchName} onChange={handleSearchName}/>
             </label>
             <FormControl sx={{m: 1, width: 300}}>
                 <InputLabel>Choose category</InputLabel>
