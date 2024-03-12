db053e6fcaa6fd3684e70caf816d95b5e9fb7ef8
jessi <jessi.nowski@hotmail.de>
2024-03-12 10:23:15 +0100

    updated new main

--- frontend/src/components/FilteredWorkouts.tsx
+++ frontend/src/components/FilteredWorkouts.tsx
@@ -42,6 +42,7 @@ export default function FilteredWorkouts(props: Readonly<FilterWorkoutsProps>){
                 <input type={"text"} value={searchName} onChange={handleSearchName}/>
                 <input type={"search"} value={searchName} onChange={handleSearchName}/>
                 <input type={"text"} value={searchName} onChange={handleSearchName}/>
+                <input type={"search"} value={searchName} onChange={handleSearchName}/>
             </label>
             <FormControl sx={{m: 1, width: 300}}>
                 <InputLabel>Choose category</InputLabel>
