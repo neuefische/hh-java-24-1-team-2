363b88eb0781de9039c68c8ae1719fd1420c951f
jessi <jessi.nowski@hotmail.de>
2024-03-12 15:07:54 +0100

    sort alphabetical

--- frontend/src/components/FilteredWorkouts.tsx
+++ frontend/src/components/FilteredWorkouts.tsx
@@ -5,29 +5,37 @@ import {Autocomplete, TextField} from "@mui/material";
 import {ChangeEvent, useState} from "react";
 import {Link} from "react-router-dom";
 
-export type FilterWorkoutsProps={
-    workouts:Workout[];
+export type FilterWorkoutsProps = {
+    workouts: Workout[];
 }
-export default function FilteredWorkouts(props: Readonly<FilterWorkoutsProps>){
-    const [searchName, setSearchName]=useState<string>("");
-    const [searchCategory, setSearchCategory]=useState<string>("");
-    const [searchMuscle, setSearchMuscle]=useState<string>("");
+export default function FilteredWorkouts(props: Readonly<FilterWorkoutsProps>) {
+    const [searchName, setSearchName] = useState<string>("");
+    const [searchCategory, setSearchCategory] = useState<string>("");
+    const [searchMuscle, setSearchMuscle] = useState<string>("");
 
-    const optionalCategories=Object.values(SportsCategory);
-    const optionalMuscles=Object.values(MuscleGroup);
+    const optionalCategories = Object.values(SportsCategory);
+    const optionalMuscles = Object.values(MuscleGroup);
 
-    function handleSearchName(e: ChangeEvent<HTMLInputElement>){
+    function handleSearchName(e: ChangeEvent<HTMLInputElement>) {
         const value = e.target.value;
         setSearchName(value);
     }
 
-    const filteredWorkouts=props.workouts.filter(workout=>
-        workout.name.includes(searchName) &&
-        workout.categories.toString().includes(searchCategory) &&
-        workout.muscleGroups.toString().includes(searchMuscle));
+    const filteredWorkouts = props.workouts.sort(function (a, b) {
+                                                            if (a.name < b.name) {
+                                                                return -1;
+                                                            }
+                                                            if (a.name > b.name) {
+                                                                return 1;
+                                                            }
+                                                            return 0;})
+                                                      .filter(workout =>
+                                                          workout.name.toLowerCase().includes(searchName) &&
+                                                          workout.categories.toString().includes(searchCategory) &&
+                                                          workout.muscleGroups.toString().includes(searchMuscle));
 
 
-    return(
+    return (
         <>
             <label>
                 Search for:
@@ -91,13 +99,13 @@ export default function FilteredWorkouts(props: Readonly<FilterWorkoutsProps>){
                 options={optionalCategories}
                 onInputChange={(_e, value) => setSearchCategory(value)}
                 sx={{m: 1, width: 300}}
-                renderInput={(params) => <TextField {...params} label="Choose Category" />}/>
+                renderInput={(params) => <TextField {...params} label="Choose Category"/>}/>
             <Autocomplete
                 disablePortal
                 options={optionalMuscles}
                 onInputChange={(_e, value) => setSearchMuscle(value)}
                 sx={{m: 1, width: 300}}
-                renderInput={(params) => <TextField {...params} label="Choose Muscle" />}/>
+                renderInput={(params) => <TextField {...params} label="Choose Muscle"/>}/>
             <ul>
                 {filteredWorkouts.map(workout => (
                     <li key={workout.id}>
