0e397a4d08bd5e69ba0afaf235d5455e7a1091f0
jessi <jessi.nowski@hotmail.de>
2024-03-13 20:06:45 +0100

    finally fixed size of Pages

--- frontend/src/pages/DetailsPage.tsx
+++ frontend/src/pages/DetailsPage.tsx
@@ -2,6 +2,8 @@ import {Workout} from "../types/Workout.ts";
 import {useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
 import "./DetailsPage.css"
+import {Chip, IconButton} from "@mui/material";
+import {DeleteForever} from "@mui/icons-material";
 
 type DetailsPageProps={
     workouts: Workout[],
@@ -33,9 +35,9 @@ export default function DetailsPage(props: Readonly<DetailsPageProps>) {
                 <div className={"workoutDetails"}>
                     <h3>{workout.name}</h3>
                     <p>Description: {workout.description}</p>
-                    <p>Categories: {workout.categories.join(', ')}</p>
-                    <p>Muscle Groups: {workout.muscleGroups.join(', ')}</p>
-                    <button onClick={handleDelete}>Delete</button>
+                    <p>Categories: {workout.categories.map(category => <Chip label={category} size={"small"}/>)}</p>
+                    <p>Muscles: {workout.muscleGroups.map(muscle => <Chip label={muscle} size={"small"}/>)}</p>
+                    <IconButton className={"deleteButton"} onClick={handleDelete}><DeleteForever/></IconButton>
                     <button onClick={handleEdit}>Edit</button>
                 </div> :
                 <>No workout found</>
