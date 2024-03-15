d647e7e98d6a1a25cb296fdf39b324858b24ef88
aaronbecher <Aaronbecher@gmx.de>
2024-03-15 14:54:37 +0100

    fix live login

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -6,7 +6,7 @@ type LoginProps={
 export default function Login(props: Readonly<LoginProps>){
 
     function login() {
-        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080': window.location.host
+        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080': window.location.origin
 
         window.open(host + '/oauth2/authorization/github', '_self')
     }
